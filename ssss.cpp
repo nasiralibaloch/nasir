3feb984529f1f730a7c44f8f6cb2aa21b638f6e4
nasiralibaloch <70817789+nasiralibaloch@users.noreply.github.com>
2020-10-17 21:59:44 +0500

    Add files via upload

---
+++ ssss.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int i,j,num=1;
+ for(i=1;i<=3;i++)
+ {
+ 	for(j=1;j<=4;j++)
+ 	{
+ 		cout<<i<<"\t"<<j<<"\n";
+	 }
+	// cout<<endl;
+ }
+ 
+ 
+ 
+      return 0;
+}
